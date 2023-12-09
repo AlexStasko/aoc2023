e893a309cae2b59660aeee07b8e7e1cc5073fd87
Aleksandr Stasko <aleksandr.stasko@burberry.com>
2023-12-09 12:04:26 +0400

    day 9

---
+++ day09/task1.ts
@@ -0,0 +1,52 @@
+import { readFileSync } from "fs";
+import path from "path";
+
+export function readFile(): string[] {
+  return readFileSync(path.join(__dirname, 'input.txt')).toString().trimEnd().split('\n');
+}
+
+export function compute(lines: string[]): number {
+  let result = 0;
+
+  result = lines.map(line => {
+    return line.split(' ').map(a => Number.parseInt(a));
+  })
+    .map(seq => {
+      let current = seq;
+      let extra: Array<Array<number>> = [];
+      let index = 0;
+      while (!current.every(a => a === 0)) {
+        for (let i = 0; i < current.length - 1; i++) {
+          if (!extra[index]) extra[index] = [];
+          extra[index].push(current[i + 1] - current[i]);
+        }
+        current = extra[index];
+        index++;
+      }
+
+      for (let i = extra.length - 2; i >= 0; i--) {
+        extra[i].push(extra[i + 1][extra[i + 1].length - 1] + extra[i][extra[i].length - 1]);
+      }
+
+      return seq[seq.length - 1] + extra[0][extra[0].length - 1];
+    })
+    .reduce((a, b) => { return a + b }, result);
+
+  return result;
+}
+
+export const INPUT = `\
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+`;
+
+export const EXPECT = 114;
+
+test('test', () => {
+  expect(compute(INPUT.trimEnd().split('\n'))).toEqual(EXPECT);
+});
+
+test('task', () => {
+  console.log('result: ', compute(readFile()));
+});
